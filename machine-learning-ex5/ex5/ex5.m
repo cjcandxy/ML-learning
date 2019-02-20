11d4d1004587510d3c07f7f72fbb9e49410d38c4
cjcandxy <952986480@qq.com>
2019-02-20 19:10:06 +0800

    ex5

--- machine-learning-ex5/ex5/ex5.m
+++ machine-learning-ex5/ex5/ex5.m
@@ -49,7 +49,6 @@ pause;
 
 theta = [1 ; 1];
 J = linearRegCostFunction([ones(m, 1) X], y, theta, 1);
-
 fprintf(['Cost at theta = [1 ; 1]: %f '...
          '\n(this value should be about 303.993192)\n'], J);
 
